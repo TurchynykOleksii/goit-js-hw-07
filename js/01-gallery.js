aaa591e2ff4e44843f23ad7f3914e974e1d0a00b
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2022-10-25 20:41:44 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const gallaryContainer = document.querySelector(".gallery");
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+gallaryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+gallaryContainer.addEventListener("click", onGallaryContainerClick);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGallaryContainerClick(e) {
+  e.preventDefault();
+  const isColorSwatchEl = e.target.dataset.source;
+  console.log(isColorSwatchEl);
+  if (!isColorSwatchEl) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${isColorSwatchEl}" width="800" height="600">
+    </div>`);
+  instance.show();
+
+  document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    if (key === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryContainer = document.querySelector(".gallery");
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+gallaryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+gallaryContainer.addEventListener("click", onGallaryContainerClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  enableKeyboard: true,
+});
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}"
+       alt="${description}"
+       title="${description}"/>
+    </a> `;
+    })
+    .join("");
+}
+
+function onGallaryContainerClick(e) {
+  e.preventDefault();
+}
